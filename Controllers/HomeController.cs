4acde63ffa94071fdab74cec1660c180ed97385d
Eric Perez <eric.t.perez1@outlook.com>
2020-11-02 08:25:47 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VoterRank.V3.Models;
+
+namespace VoterRank.V3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }        
+        public IActionResult Contact()
+        {
+            return View();
+        }        
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult VotingSection()
+        {
+            return View();
+        }        
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
